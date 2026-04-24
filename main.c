871f0a5513dd0421710ad600629c0b9469b279da
Pedro Zagato <117411272+pzgfps@users.noreply.github.com>
2026-04-23 23:34:33 -0300

    Refactor voting percentage calculations and outputs

--- main.c
+++ main.c
@@ -15,10 +15,11 @@ para demonstrar quantos votantes protestaram na seção eleitoral.
 
 *******************************************************************************/
 #include <stdio.h>
+#include <string.h>
 
 int main()
 {
-    char nomeCandidato1[50], nomeCandidato2[50], nomeCandidato3[50], maisVotados[1];
+    char nomeCandidato1[50], nomeCandidato2[50], nomeCandidato3[50], maisVotado1[50], maisVotado2[50];
     int numeroEleitoral1, numeroEleitoral2, numeroEleitoral3;
     float qntdVotosBrancos, qntdVotosNulos, qntdVotosTotal, qntdVotosValidos, qntdVotos1, qntdVotos2, qntdVotos3;
     float porcentagemVotos1 = 0, porcentagemVotos2 = 0, porcentagemVotos3 = 0, aprovado = 50.0;
@@ -52,28 +53,64 @@ int main()
     qntdVotosTotal = qntdVotos1 + qntdVotos2 + qntdVotos3 + qntdVotosNulos + qntdVotosBrancos;
     qntdVotosValidos = qntdVotos1 + qntdVotos2 + qntdVotos3;
     
-    porcentagemVotos1 = (qntdVotos1 / qntdVotosTotal) * 100;
-    porcentagemVotos2 = (qntdVotos2 / qntdVotosTotal) * 100;
-    porcentagemVotos3 = (qntdVotos3 / qntdVotosTotal) * 100;
+    porcentagemVotos1 = (qntdVotos1 / qntdVotosValidos) * 100;
+    porcentagemVotos2 = (qntdVotos2 / qntdVotosValidos) * 100;
+    porcentagemVotos3 = (qntdVotos3 / qntdVotosValidos) * 100;
     
     printf("\nQuantidade total de votos: %.0f", qntdVotosTotal);
+    printf("\nVotos brancos: %.2f%% do total", (qntdVotosBrancos / qntdVotosTotal) * 100);
+    printf("\nVotos nulos:   %.2f%% do total", (qntdVotosNulos   / qntdVotosTotal) * 100);
     printf("\nQuantidade de votos válidos: %.0f", qntdVotosValidos);
-    printf("\n%s: %.2f% votos", nomeCandidato1, porcentagemVotos1);
-    printf("\n%s: %.2f% votos", nomeCandidato2, porcentagemVotos2);
-    printf("\n%s: %.2f% votos", nomeCandidato3, porcentagemVotos3);
+    printf("\n\n%s: %.2f%% votos", nomeCandidato1, porcentagemVotos1);
+    printf("\n%s: %.2f%% votos", nomeCandidato2, porcentagemVotos2);
+    printf("\n%s: %.2f%% votos", nomeCandidato3, porcentagemVotos3);
     
     if(porcentagemVotos1 > aprovado) {
-        printf("\n%s foi eleito com %.1f% total de votos!", nomeCandidato1, porcentagemVotos1);
+        printf("\n\n%s foi eleito com %.2f%% dos votos validos!", nomeCandidato1, porcentagemVotos1);
     } else if(porcentagemVotos2 > aprovado) {
-        printf("\n%s foi eleito com %.1f% total de votos!", nomeCandidato2, porcentagemVotos2);
+        printf("\n\n%s foi eleito com %.2f%% dos votos validos!", nomeCandidato2, porcentagemVotos2);
     } else if (porcentagemVotos3 > aprovado) {
-        printf("\n%s foi eleito com %.1f% total de votos!", nomeCandidato3, porcentagemVotos3);
+        printf("\n\n%s foi eleito com %.2f%% dos votos validos!", nomeCandidato3, porcentagemVotos3);
     } else {
-        
-        printf("\nNão houve eleitos no 1° turno!");
+        int numMaisVotado1, numMaisVotado2;
+        float votosMaisVotado1, votosMaisVotado2;
+
+    if (qntdVotos1 >= qntdVotos2 && qntdVotos1 >= qntdVotos3) {
+        strcpy(maisVotado1, nomeCandidato1);
+        votosMaisVotado1 = qntdVotos1;
+        numMaisVotado1 = numeroEleitoral1;
+    } else if (qntdVotos2 >= qntdVotos1 && qntdVotos2 >= qntdVotos3) {
+        strcpy(maisVotado1, nomeCandidato2);
+        votosMaisVotado1 = qntdVotos2;
+        numMaisVotado1 = numeroEleitoral2;
+    } else {
+        strcpy(maisVotado1, nomeCandidato3);
+        votosMaisVotado1 = qntdVotos3;
+        numMaisVotado1 = numeroEleitoral3;
     }
-    
-    
+    if (numMaisVotado1 == numeroEleitoral1) {
+        if (qntdVotos2 >= qntdVotos3) {
+            strcpy(maisVotado2, nomeCandidato2);
+        } else {
+            strcpy(maisVotado2, nomeCandidato3);
+        }
+    } else if (numMaisVotado1 == numeroEleitoral2) {
+        if (qntdVotos1 >= qntdVotos3) {
+            strcpy(maisVotado2, nomeCandidato1);
+        } else {
+            strcpy(maisVotado2, nomeCandidato3);
+        }
+    } else {
+        if (qntdVotos1 >= qntdVotos2) {
+            strcpy(maisVotado2, nomeCandidato1);
+        } else {
+            strcpy(maisVotado2, nomeCandidato2);
+        }
+    }
+    printf("\n\nNenhum candidato atingiu 50%% dos votos validos.\n");
+    printf("2° turno entre: %s  e %s\n", maisVotado1, maisVotado2);
+
+    }
     
     return 0;
 }
